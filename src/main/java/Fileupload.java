63a3f3c55b39412318ce8df29b93297746c1836c
dhananjayppatankar <dhananjayppatankar@gmail.com>
2017-10-30 17:15:15 -0400

    Added Apache POI library to read the file and console log it...

--- src/main/java/Fileupload.java
+++ src/main/java/Fileupload.java
@@ -1,6 +1,7 @@
 package main.java;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 @WebServlet("/choose")
 public class Fileupload extends HttpServlet {
@@ -28,15 +32,21 @@ public class Fileupload extends HttpServlet {
 
 			for (FileItem item : multifiles) {
 
-				item.write(new File("C:/Users/dhana/git/XLFileReader"+ item.getName()));
+				item.write(new File("C:/Users/dhana/git/XLFileReader" + item.getName()));
 			}
 
+			System.out.println("Choose servlet called !! ");
+
+			XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream("C:/Users/dhana/git/XLFileReaderdhan.xlsx"));
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			XSSFRow row = sheet.getRow(0);
+			System.out.println(row.getCell(0).getNumericCellValue() + row.getCell(1).getStringCellValue());
+			
 		} catch (Exception e) {
 
 			e.printStackTrace();
 		}
 
-		System.out.println("Choose servlet called !! ");
 	}
 
 }
